a0c3c4abb4889942008577f1fedab9fadd7e2d97
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-01-24 23:26:37 -0800

    Add REM sleep data (#2)
    * Create README.md
    
    * Add REM data

--- Charter.Tests/BasicTests.cs
+++ Charter.Tests/BasicTests.cs
@@ -27,15 +27,34 @@ namespace Charter.Tests
         }
 
         [TestMethod]
+        public void HasREMSleepData()
+        {
+            Assert.IsNotNull(SampleData.MinutesOfREMSleep);
+            Assert.IsTrue(SampleData.MinutesOfREMSleep.Count > 0);
+        }
+
+        [TestMethod]
         public void AllDeepSleepPointsHaveAForegroundColor()
         {
             Assert.IsFalse(SampleData.MinutesOfDeepSleep.Any(d => d.Foreground == null));
         }
 
         [TestMethod]
+        public void AllREMSleepPointsHaveAForegroundColor()
+        {
+            Assert.IsFalse(SampleData.MinutesOfREMSleep.Any(d => d.Foreground == null));
+        }
+
+        [TestMethod]
         public void AllDeepSleepPointsHaveALabel()
         {
             Assert.IsFalse(SampleData.MinutesOfDeepSleep.Any(d => d.Label == null));
+        }
+
+        [TestMethod]
+        public void AllREMSleepPointsHaveALabel()
+        {
+            Assert.IsFalse(SampleData.MinutesOfREMSleep.Any(d => d.Label == null));
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+![Screenshot 2025-01-24 231559](https://github.com/user-attachments/assets/da22f596-38ee-4955-acb1-8ff390f70543)
+
+Charter is a simple charting project being used to experiment with GitHub Actions.
