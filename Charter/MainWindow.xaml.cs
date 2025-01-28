33e945567c1ebc01dce85e0ba5e5dcddcf00ffa0
CascadePass <106619481+CascadePass@users.noreply.github.com>
2025-01-27 17:47:50 -0800

    Use LiveCharts library
    Also add sample data to chart.

--- Charter/MainWindow.xaml.cs
+++ Charter/MainWindow.xaml.cs
@@ -8,6 +8,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using LiveCharts.Charts;
+using LiveCharts.Configurations;
 
 namespace Charter
 {
@@ -18,14 +22,55 @@ namespace Charter
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
             this.DataContext = this;
-
-            this.MinutesOfDeepSleep = SampleData.MinutesOfDeepSleep;
-            this.MinutesOfREMSleep = SampleData.MinutesOfREMSleep;
         }
 
-        public List<DataPoint> MinutesOfDeepSleep { get; set; }
-        public List<DataPoint> MinutesOfREMSleep { get; set; }
+
+        public List<string> Labels => SampleData.MinutesOfDeepSleep.Select(d => d.Label).ToList();
+
+        public SeriesCollection Series
+        {
+            get
+            {
+                SeriesCollection result = new();
+
+                ColumnSeries deepSleepGarminSeries = new()
+                {
+                    Title = "Deep Sleep (Garmin)",
+                    Values = new ChartValues<int>(SampleData.MinutesOfDeepSleep.Where(src => src.Foreground == Brushes.Green).Select(d => d.Value)),
+                    Fill = Brushes.Yellow,
+                };
+
+                ColumnSeries remSleepGarminSeries = new()
+                {
+                    Title = "REM (Garmin)",
+                    Values = new ChartValues<int>(SampleData.MinutesOfREMSleep.Where(src => src.Foreground == Brushes.Green).Select(d => d.Value)),
+                    Fill = Brushes.Orange,
+                };
+
+
+                ColumnSeries deepSleepMuseSeries = new()
+                {
+                    Title = "Deep Sleep (Muse)",
+                    Values = new ChartValues<int>(SampleData.MinutesOfDeepSleep.Where(src => src.Foreground == Brushes.Red).Select(d => d.Value)),
+                    Fill = Brushes.BlueViolet,
+                };
+
+                ColumnSeries remSleepMuseSeries = new()
+                {
+                    Title = "REM (Muse)",
+                    Values = new ChartValues<int>(SampleData.MinutesOfREMSleep.Where(src => src.Foreground == Brushes.Red).Select(d => d.Value)),
+                    Fill = Brushes.Red,
+                };
+
+                result.Add(deepSleepGarminSeries);
+                result.Add(remSleepGarminSeries);
+                result.Add(deepSleepMuseSeries);
+                result.Add(remSleepMuseSeries);
+
+                return result;
+            }
+        }
     }
 }
